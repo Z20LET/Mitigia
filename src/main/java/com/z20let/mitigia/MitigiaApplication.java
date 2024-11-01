fc93bed17491eeb516e789ef17a469c4f0b23f49
Z20LET <trautmann.marton@outlook.com>
2024-11-01 17:57:55 +0100

    Init Project

---
+++ src/main/java/com/z20let/mitigia/MitigiaApplication.java
@@ -0,0 +1,13 @@
+package com.z20let.mitigia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitigiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitigiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/z20let/mitigia/MitigiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z20let.mitigia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitigiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
