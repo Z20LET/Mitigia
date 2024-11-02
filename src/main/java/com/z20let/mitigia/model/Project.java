cb4f7019115f7cae077d08a360bcc50c289372a2
Z20LET <trautmann.marton@outlook.com>
2024-11-02 19:05:59 +0100

    Create Refined Classes

--- src/main/java/com/z20let/mitigia/model/Project.java
+++ src/main/java/com/z20let/mitigia/model/Project.java
@@ -1,15 +1,11 @@
 package com.z20let.mitigia.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.OffsetDateTime;
-import java.util.Date;
-
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -18,31 +14,16 @@ import java.util.Date;
 @Entity
 public class Project {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer projectId;
-    private String licensePlate;
-    private Long vehicleId;
-    private Date startDate;
-    private OffsetDateTime endDate;
-    private Integer startOdometer;
-    private Integer endOdometer;
-
-    public Integer getOdometer() {
-        return endOdometer;
-    }
-
-    public void setOdometer(Integer odometer) {
-        this.endOdometer = odometer;
-    }
-
-    public OffsetDateTime getDate() {
-        return endDate;
-    }
-
-    public void setDate(OffsetDateTime date) {
-        this.endDate = date;
-    }
-
-    public void setEndDate(Date dateCellValue) {
-        this.endDate = dateCellValue.toInstant().atOffset(OffsetDateTime.now().getOffset());
-    }
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Integer startMileage;
+    private Integer endMileage;
+    private Double carbonEmission;
+    @OneToOne
+    @JoinColumn(name = "vehicle_id")
+    Vehicle vehicle;
 }
+
+
